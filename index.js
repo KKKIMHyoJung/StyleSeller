210ee3061e2d5ca3cc3d533d5ccb12b701efb5d1
KKKIMHyoJung <154449017+KKKIMHyoJung@users.noreply.github.com>
2023-12-21 10:11:40 +0900

    현재까지 완료/진행중인 프로젝트
    [ 완료 ]
    section1-4 완료
    
    [ 진행중 ]
    section5 - 마우스호버/반응형

---
+++ index.js
@@ -0,0 +1,186 @@
+
+
+
+/* #############################################
+sec1 -> .hamBtn | 모바일 화면시 햄버거 버튼 활성화
+################################################*/
+$(function(){
+
+    let xBtn = $(".xBtn");
+    let hamBtn = $(".hamBtn");
+    let hamburger = $("#hamburger");
+
+    hamburger.hide();
+
+    hamBtn.click(function(){
+        hamburger.show();
+        hamBtn.hide();
+        $("body").addClass("hidden_content");
+    });
+
+    xBtn.click(function(){
+        hamburger.hide();
+        hamBtn.show();
+        $("body").removeClass("hidden_content");
+    });
+
+
+    let main1 = $("#main1");
+    let main2 = $("#main2");
+    let main1Btn = $(".moveBtn1");
+    let main2Btn = $(".moveBtn2");
+    let purpleBox = $(".purpleBox");
+    
+
+    main2.hide();
+
+    main1Btn.click(function(){
+        main2.show();
+        main1.hide();
+        purpleBox.addClass("purpleani");
+
+    });
+
+    main2Btn.click(function(){
+        main2.hide();
+        main1.show();
+
+    });
+})
+
+
+
+
+/* ##################################################
+sec3 -> 마우스 휠 이벤트 (#sec3img -> oevrflos:heddon)
+#####################################################*/
+
+
+
+
+/* ##################################################
+sec3 -> 글씨 토글
+#####################################################*/
+
+$(function(){
+    let text_btn = $(".sec3_txt_header");
+    let show_txt = $(".sec3_txt_pupletxt");
+    let show_txt2 = $(".sec3_txt_txt");
+    let show_circle = $(".sec3_circles .sec3circle");
+
+    show_txt.eq(1).hide();
+    show_txt.eq(2).hide();
+    show_txt2.eq(1).hide();
+    show_txt2.eq(2).hide();
+    show_circle.hide();
+
+
+    text_btn.eq(0).click(function(){
+        show_txt.eq(0).show();
+        show_txt2.eq(0).show(); 
+
+        show_txt.eq(1).hide();
+        show_txt2.eq(1).hide();  
+        show_circle.hide();  
+        show_txt.eq(2).hide();
+        show_txt2.eq(2).hide();
+
+        text_btn.eq(0).addClass("active_toggle");        
+
+        text_btn.eq(1).removeClass("active_toggle");
+        text_btn.eq(2).removeClass("active_toggle");
+
+    })
+    
+    text_btn.eq(1).click(function(){
+        show_txt.eq(1).show();
+        show_txt2.eq(1).show();  
+        show_circle.show();
+
+        show_txt.eq(0).hide();
+        show_txt2.eq(0).hide(); 
+        show_txt.eq(2).hide();
+        show_txt2.eq(2).hide();
+
+        text_btn.eq(1).addClass("active_toggle");
+
+        text_btn.eq(0).removeClass("active_toggle");
+        text_btn.eq(2).removeClass("active_toggle");
+
+    })
+
+    text_btn.eq(2).click(function(){
+        show_txt.eq(2).show();
+        show_txt2.eq(2).show();
+
+        show_txt.eq(0).hide();
+        show_txt2.eq(0).hide();  
+        show_circle.hide();  
+        show_txt.eq(1).hide();
+        show_txt2.eq(1).hide();  
+
+        text_btn.eq(2).addClass("active_toggle");
+
+        text_btn.eq(0).removeClass("active_toggle");
+        text_btn.eq(1).removeClass("active_toggle");        
+    })
+
+})
+
+
+/* #############################################
+sec4 -> 숫자 카운트 애니메이션
+################################################*/
+
+
+
+$(document).ready(function () {
+
+    function animateCounter(element, targetCount) {
+        $({ count: 0 }).animate(
+            {
+                count: targetCount,
+            },
+            {
+                duration: 2000,
+                easing: 'swing',
+                step: function () {
+                    element.text(Math.floor(this.count));
+                },
+                complete: function () {
+                    element.text(targetCount);
+                },
+            }
+        );
+    }
+
+    function checkScroll() {
+        // 현재 스크롤 위치
+        var scrollPosition = $(window).scrollTop();
+
+        // #sec4 요소의 상단 위치
+        var targetPosition = $('#sec4').offset().top;
+
+        // #sec4의 상단 위치에 도달하면 애니메이션 실행
+        if (scrollPosition >= targetPosition) {
+            const targetElement = $('.sec4_count');
+            const targetCount1 = 12600;
+            animateCounter(targetElement, targetCount1);
+
+            const targetElement2 = $('.sec4_count:eq(1)');
+            const targetCount2 = 320;
+            animateCounter(targetElement2, targetCount2);
+
+            const targetElement3 = $('.sec4_count:last');
+            const targetCount3 = 943;
+            animateCounter(targetElement3, targetCount3);
+
+        }
+    }
+
+    // 스크롤 이벤트를 감지하여 checkScroll 함수 호출
+    $(window).on('scroll', checkScroll);
+
+    // 초기 로드시 한 번 체크
+    checkScroll();
+});
