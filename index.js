d756a6b5967ff82b82243d7344c4f4c4687a64c0
KKKIMHyoJung <154449017+KKKIMHyoJung@users.noreply.github.com>
2023-12-22 09:25:35 +0900

    [update] section5
    updated section5 with midea query, mouse hover effect and button event(open, close)

--- index.js
+++ index.js
@@ -138,19 +138,19 @@ $(document).ready(function () {
 
     function animateCounter(element, targetCount) {
         $({ count: 0 }).animate(
-            {
-                count: targetCount,
+        {
+            count: targetCount,
+        },
+        {
+            duration: 2000,
+            easing: 'swing',
+            step: function () {
+                element.text(Math.floor(this.count));
             },
-            {
-                duration: 2000,
-                easing: 'swing',
-                step: function () {
-                    element.text(Math.floor(this.count));
-                },
-                complete: function () {
-                    element.text(targetCount);
-                },
-            }
+            complete: function () {
+                element.text(targetCount);
+            },
+        }
         );
     }
 
@@ -184,3 +184,332 @@ $(document).ready(function () {
     // 초기 로드시 한 번 체크
     checkScroll();
 });
+
+
+
+
+/* ##################################################
+sec5 -> card 호버
+#####################################################*/
+
+
+
+$(function () {
+
+    let cards = [];
+    let hovers = [];
+    let hideElements = [];
+
+    for (let i = 1; i <= 4; i++) {
+        cards[i] = $(`#sec5 .sec5_card_wapper .sec5_card:nth-of-type(${2 * i - 1})`);
+        hovers[i] = $(`#sec5 .sec5_card_wapper .sec5_card_hover:nth-of-type(${2 * i})`);
+        hideElements[i] = $(`#sec5 .sec5_card_wapper .sec5_card:nth-of-type(${2 * i - 1}) .sec5_card_title, #sec5 .sec5_card_wapper .sec5_card:nth-of-type(${2 * i - 1}) .sec5_card_seller`);
+    }
+
+ 
+    let openBtns = $(".sec5_open_btn");
+    let closeBtns = $(".sec5_close_btn");
+
+
+    for (let i = 1; i <= 4; i++) {
+        hovers[i].hide();
+    }
+
+    function handleCardHover(card, hover, hideElements) {
+        hover.show();
+        hideElements.hide();
+    }
+
+
+    function handleCardLeave(hover, hideElements) {
+        hover.hide();
+        hideElements.show();
+    }
+
+
+    function bindCardEvents(card, hover, hideElements) {
+        card.add(hover).on("mouseover", function () {
+            handleCardHover(card, hover, hideElements);
+        }).on("mouseleave", function () {
+            handleCardLeave(hover, hideElements);
+        });
+    }
+
+    function updateHoverState() {
+        let isHoverEnabled = window.innerWidth > 767;
+
+        for (let i = 1; i <= 4; i++) {
+            if (isHoverEnabled) {
+                bindCardEvents(cards[i], hovers[i], hideElements[i]);
+            } else {
+                cards[i].off("mouseover mouseleave");
+                hovers[i].hide();
+                hideElements[i].show();
+            }
+        }
+    }
+
+
+    updateHoverState();
+
+    $(window).on('resize', function () {
+        updateHoverState();
+    });
+
+
+    openBtns.each(function (index) {
+        $(this).click(function () {
+            hovers[index + 1].show();
+        });
+    });
+
+    closeBtns.each(function (index) {
+        $(this).click(function () {
+            hovers[index + 1].hide();
+        });
+    });
+});
+
+
+
+// $(function () {
+
+
+//     let card1 = $("#sec5 .sec5_card_wapper .sec5_card:nth-of-type(1)");
+//     let card2 = $("#sec5 .sec5_card_wapper .sec5_card:nth-of-type(3)");
+//     let card3 = $("#sec5 .sec5_card_wapper .sec5_card:nth-of-type(5)");
+//     let card4 = $("#sec5 .sec5_card_wapper .sec5_card:nth-of-type(7)");
+
+//     let hover_1 = $("#sec5 .sec5_card_wapper .sec5_card_hover:nth-of-type(2)");
+//     let hover_2 = $("#sec5 .sec5_card_wapper .sec5_card_hover:nth-of-type(4)");
+//     let hover_3 = $("#sec5 .sec5_card_wapper .sec5_card_hover:nth-of-type(6)");
+//     let hover_4 = $("#sec5 .sec5_card_wapper .sec5_card_hover:nth-of-type(8)");
+
+//     let hide_elements1 = $("#sec5 .sec5_card_wapper .sec5_card:nth-of-type(1) .sec5_card_title,#sec5 .sec5_card_wapper .sec5_card:nth-of-type(1) .sec5_card_seller");
+//     let hide_elements2 = $("#sec5 .sec5_card_wapper .sec5_card:nth-of-type(3) .sec5_card_title,#sec5 .sec5_card_wapper .sec5_card:nth-of-type(3) .sec5_card_seller");
+//     let hide_elements3 = $("#sec5 .sec5_card_wapper .sec5_card:nth-of-type(5) .sec5_card_title,#sec5 .sec5_card_wapper .sec5_card:nth-of-type(5) .sec5_card_seller");
+//     let hide_elements4 = $("#sec5 .sec5_card_wapper .sec5_card:nth-of-type(7) .sec5_card_title,#sec5 .sec5_card_wapper .sec5_card:nth-of-type(7) .sec5_card_seller");
+
+ 
+//     let open_btn = $(".sec5_open_btn");
+//     let close_btn = $(".sec5_close_btn");
+
+
+//     hover_1.hide();
+//     hover_2.hide();
+//     hover_3.hide();
+//     hover_4.hide();
+
+
+
+//     function it(card, hover, hideElements) {
+//         hover.show();
+//         hideElements.hide();
+//     }
+
+//     function handleCardLeave(hover, hideElements) {
+//         hover.hide();
+//         hideElements.show();
+//     }
+
+//     function bindCardEvents(card, hover, hideElements) {
+//         card.add(hover).on("mouseover", function () {
+//             handleCardHover(card, hover, hideElements);
+//         }).on("mouseleave", function () {
+//             handleCardLeave(hover, hideElements);
+//         });
+//     }
+
+//     function updateHoverState() {
+//         if (window.innerWidth < 767) {
+//             // Enable hover and disable button functionality
+//             card1.off("mouseover mouseleave");
+//             hover_1.hide();
+//             hide_elements1.show();
+//         } else {
+//             // Disable hover and enable button functionality
+//             bindCardEvents(card1, hover_1, hide_elements1);
+//         }
+//     }
+
+//     updateHoverState();
+
+//     $(window).on('resize', function () {
+//         updateHoverState();
+//     });
+
+//     open_btn1.click(function () {
+//         hover_1.show();
+//     });
+
+//     close_btn1.click(function () {
+//         hover_1.hide();
+//     });
+// });
+
+
+// $(function(){
+
+//     let card1 = $("#sec5 .sec5_card_wapper .sec5_card:nth-of-type(1)");
+//     let card2 = $("#sec5 .sec5_card_wapper .sec5_card:nth-of-type(3)");
+//     let card3 = $("#sec5 .sec5_card_wapper .sec5_card:nth-of-type(5)");
+//     let card4 = $("#sec5 .sec5_card_wapper .sec5_card:nth-of-type(7)");
+
+//     let hover_1 = $("#sec5 .sec5_card_wapper .sec5_card_hover:nth-of-type(2)");
+//     let hover_2 = $("#sec5 .sec5_card_wapper .sec5_card_hover:nth-of-type(4)");
+//     let hover_3 = $("#sec5 .sec5_card_wapper .sec5_card_hover:nth-of-type(6)");
+//     let hover_4 = $("#sec5 .sec5_card_wapper .sec5_card_hover:nth-of-type(8)");
+
+//     let hide_elements1 = $("#sec5 .sec5_card_wapper .sec5_card:nth-of-type(1) .sec5_card_title,#sec5 .sec5_card_wapper .sec5_card:nth-of-type(1) .sec5_card_seller");
+//     let hide_elements2 = $("#sec5 .sec5_card_wapper .sec5_card:nth-of-type(3) .sec5_card_title,#sec5 .sec5_card_wapper .sec5_card:nth-of-type(3) .sec5_card_seller");
+//     let hide_elements3 = $("#sec5 .sec5_card_wapper .sec5_card:nth-of-type(5) .sec5_card_title,#sec5 .sec5_card_wapper .sec5_card:nth-of-type(5) .sec5_card_seller");
+//     let hide_elements4 = $("#sec5 .sec5_card_wapper .sec5_card:nth-of-type(7) .sec5_card_title,#sec5 .sec5_card_wapper .sec5_card:nth-of-type(7) .sec5_card_seller");
+
+
+//     let open_btn = $(".sec5_open_btn");
+//     let close_btn = $(".sec5_close_btn");
+
+//     hover_1.hide();
+//     hover_2.hide();
+//     hover_3.hide();
+//     hover_4.hide();
+
+
+
+//     open_btn.eq(0).click(function(){
+
+//         hover_1.show();
+//     })
+//     close_btn.eq(0).click(function(){
+
+//         hover_1.hide();
+//     })
+
+
+//     open_btn.eq(1).click(function(){
+
+//         hover_2.show();
+//     })
+//     close_btn.eq(1).click(function(){
+
+//         hover_2.hide();
+//     })
+
+
+//     open_btn.eq(2).click(function(){
+
+//         hover_3.show();
+//     })
+//     close_btn.eq(2).click(function(){
+
+//         hover_3.hide();
+//     })
+
+
+//     open_btn.eq(3).click(function(){
+
+//         hover_4.show();
+//     })
+//     close_btn.eq(3).click(function(){
+
+//         hover_4.hide();
+//     })
+
+
+
+// });
+
+
+
+// $(document).ready(function () {
+//     let isHoverEnabled = true;
+
+//     function updateHoverState() {
+//         if (window.innerWidth <= 767) {
+//             isHoverEnabled = false;
+//         } else {
+//             isHoverEnabled = true;
+//         }
+//     }
+
+//     function handleCardHover(card, hover, hideElements) {
+//         if (isHoverEnabled) {
+//             hover.show();
+//             hideElements.hide();
+//         }
+//     }
+
+//     function handleCardLeave(hover, hideElements) {
+//         hover.hide();
+//         hideElements.show();
+//     }
+
+//     let card1 = $("#sec5 .sec5_card_wapper .sec5_card:nth-of-type(1)");
+//     let card2 = $("#sec5 .sec5_card_wapper .sec5_card:nth-of-type(3)");
+//     let card3 = $("#sec5 .sec5_card_wapper .sec5_card:nth-of-type(5)");
+//     let card4 = $("#sec5 .sec5_card_wapper .sec5_card:nth-of-type(7)");
+
+//     let hover_1 = $("#sec5 .sec5_card_wapper .sec5_card_hover:nth-of-type(2)");
+//     let hover_2 = $("#sec5 .sec5_card_wapper .sec5_card_hover:nth-of-type(4)");
+//     let hover_3 = $("#sec5 .sec5_card_wapper .sec5_card_hover:nth-of-type(6)");
+//     let hover_4 = $("#sec5 .sec5_card_wapper .sec5_card_hover:nth-of-type(8)");
+
+//     let hide_elements1 = $("#sec5 .sec5_card_wapper .sec5_card:nth-of-type(1) .sec5_card_title, #sec5 .sec5_card_wapper .sec5_card:nth-of-type(1) .sec5_card_seller");
+//     let hide_elements2 = $("#sec5 .sec5_card_wapper .sec5_card:nth-of-type(3) .sec5_card_title, #sec5 .sec5_card_wapper .sec5_card:nth-of-type(3) .sec5_card_seller");
+//     let hide_elements3 = $("#sec5 .sec5_card_wapper .sec5_card:nth-of-type(5) .sec5_card_title, #sec5 .sec5_card_wapper .sec5_card:nth-of-type(5) .sec5_card_seller");
+//     let hide_elements4 = $("#sec5 .sec5_card_wapper .sec5_card:nth-of-type(7) .sec5_card_title, #sec5 .sec5_card_wapper .sec5_card:nth-of-type(7) .sec5_card_seller");
+
+//     let open_btn = $(".sec5_open_btn");
+//     let close_btn = $(".sec5_close_btn");
+
+//     hover_1.hide();
+//     hover_2.hide();
+//     hover_3.hide();
+//     hover_4.hide();
+
+
+
+//     updateHoverState();
+
+//     $(window).on('resize', function () {
+//         updateHoverState();
+//     });
+
+//     card1.add(hover_1).on("mouseover", function () {
+//         handleCardHover(card1, hover_1, hide_elements1);
+//     }).on("mouseleave", function () {
+//         handleCardLeave(hover_1, hide_elements1);
+//     });
+
+
+
+//     card2.add(hover_2).on("mouseover", function () {
+//         handleCardHover(card2, hover_2, hide_elements2);
+//     }).on("mouseleave", function () {
+//         handleCardLeave(hover_2, hide_elements2);
+//     });
+
+//     card3.add(hover_3).on("mouseover", function () {
+//         handleCardHover(card3, hover_3, hide_elements3);
+//     }).on("mouseleave", function () {
+//         handleCardLeave(hover_3, hide_elements3);
+//     });
+
+
+
+//     card4.add(hover_4).on("mouseover", function () {
+//         handleCardHover(card4, hover_4, hide_elements4);
+//     }).on("mouseleave", function () {
+//         handleCardLeave(hover_4, hide_elements4);
+//     });
+
+
+
+
+
+// });
+
+
+
+
+
